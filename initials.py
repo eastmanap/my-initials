761497e8740cdb7c6f5e90e4c1c9f4cb298bf3b4
eastmanap <eastmanap354@northwested.org>
2025-02-14 11:19:38 -0500

    Update and rename template.py to initials.py

--- initials.py
+++ initials.py
@@ -15,7 +15,7 @@ pygame.init()
 
 # --- Screen setup --- #
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Pygame Template")
+pygame.display.set_caption("template name")
 
 # --- Clock setup --- #
 clock = pygame.time.Clock()  # Note the capital C in the word Clock!
@@ -35,7 +35,24 @@ while running:
     screen.fill(WHITE)  # Fill screen background with white
 
     # (This is where you'll draw your game objects)
+    def draw_line(coord1, coord2, color = (0, 0, 0), width = 50):
+        pygame.draw.line(screen, color, coord1, coord2, width)
 
+    # Letter A
+    draw_line((100, 100), (300, 100), (75, 204, 178))
+    draw_line((100, 100), (100, 300), (75, 204, 178))
+    draw_line((300, 100), (300, 300), (75, 204, 178))
+    draw_line((100, 300), (300, 300), (75, 204, 178))
+    draw_line((100, 300), (100, 525), (75, 204, 178))
+    draw_line((300, 300), (300, 525), (75, 204, 178))
+
+    # Letter E
+    draw_line((500, 100), (700, 100))
+    draw_line((500, 100), (500, 300))
+    draw_line((500, 300), (700, 300))
+    draw_line((500, 300), (500, 500))
+    draw_line((500, 500), (700, 500))
+    
     pygame.display.flip()  # Update the display
 
     # --- Limit frames per second (FPS) --- #
